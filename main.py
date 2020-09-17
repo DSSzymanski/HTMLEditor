cf87e5174ae73bdd1b0d409ffadd0078af6638f5
DSSzymanski <dszymanski@umass.edu>
2020-09-17 17:11:54 -0400

    altered to return array of strings by line instead of a giant string

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 def getCodeFromFile(fileLocation):
     """
-    Opens HTML file and returns code as string
+    Opens HTML file and returns code as an array of strings
     :param fileLocation: path to HTML file
     """
+    codeList = []
     with open(fileLocation, "r") as f:
-        return f.read()
+        for line in f:
+            codeList.append(line)
+    return codeList
+
+#print(getCodeFromFile("C:\\Users\\Daniel\\Desktop\\Code_Orange.html"))
