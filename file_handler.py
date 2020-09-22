3aaae41ccd63180cd0dbfce072d8468c0861b137
DSSzymanski <dszymanski@umass.edu>
2020-09-21 21:00:05 -0400

    added rest of the DFS method and write_code_to_file

--- file_handler.py
+++ file_handler.py
@@ -1,16 +1,33 @@
 """
 Code used to read/write from files.
 """
+import code_replacer as cr
+import navbar
+from shutil import copytree
 from os.path import isdir
 from os import listdir
 
+def copy_to_new_dir(src_path, dest_path):
+    copytree(src_path, dest_path)
+
 def DFS(path, ext):
+    """
+    Performs depth first search on the directory given. Searches directory for
+    all files that end with extention inputed.
+    :param path: string representing a directory OR path to search through and
+                alter code
+    :param ext: string representing extention to search for
+    """
     if isdir(path):
         for directory in listdir(path):
             DFS(path + "\\" + directory, ext)
     else:
         if path.endswith(ext):
-            print(path)
+            code = read_code_from_file(path)
+            #TODO change to tokens from empty dictionary
+            code = cr.search_and_alter_code(code, navbar.tokens)
+            write_code_to_file(code, path)
+
 
 def read_code_from_file(fileLocation):
     """
@@ -29,4 +46,5 @@ def write_code_to_file(code, fileLocation):
     :param code: list of strings representing code.
     :param fileLocation: path to write to.
     """
-    pass
+    with open(fileLocation, 'w') as f:
+        f.writelines(code)
--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import file_handler
+import file_handler as fh
+from os.path import exists
 cdur = "C:\\Users\\Daniel\\Desktop\\programming\\work\\switchboard\\code_website"
+ddur = "C:\\Users\\Daniel\\Desktop\\stuff"
 ext = ".html"
-file_handler.DFS(cdur, ext)
+if exists(cdur):
+    fh.copy_to_new_dir(cdur, ddur)
+    fh.DFS(ddur, ext)
